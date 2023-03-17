4c979a0bdbda9042fb8c96c90233669f1f383308
Denis <den290m@gmail.com>
2023-03-17 15:44:39 +0100

    Add Player class, GeneratePlayer method

--- src/Main.java
+++ src/Main.java
@@ -4,32 +4,64 @@ public class Main {
     public static void main(String[] args) {
         generateOfDeck();
 
-    }
-
-    private static void generatePlayers(){
+        generatePlayers();
 
     }
-    private static void generateOfDeck() {
-        List<Cards> deckOfCards = new ArrayList<>();
+
+    public static void generatePlayers(){
+        Player player1 = new Player();
+        player1.setName("Denis");
+        Stack<Cards> deckOfCards = new Stack<>();
+        deckOfCards.addAll(generateOfDeck());
+        for(int i = 0; i<7;i++){
+        player1.Hand.push(deckOfCards.pop());}
+        System.out.println(deckOfCards);
+        System.out.println(player1.Hand);
+
+    }
+    public static Stack<Cards> generateOfDeck() {
+        Stack<Cards> deckOfCards = new Stack<>();
         for (Cards.Face face : Cards.Face.values()) {
             for (Cards.Color color : Cards.Color.values()) {
                 if (face == Cards.Face.BLOCK || face == Cards.Face.REVERSE || face == Cards.Face.PLUS_TWO ){
-                    deckOfCards.add(new Cards(face, color));
-                    deckOfCards.add(new Cards(face, color));
+                    deckOfCards.push(new Cards(face, color));
+                    deckOfCards.push(new Cards(face, color));
                 } else
                 if (face == Cards.Face.WHITE_CHANGE_COLOR || face == Cards.Face.CHANGE_COLOR || face == Cards.Face.PLUS_TEN) {
-                    deckOfCards.add(new Cards(face, null));
+                    deckOfCards.push(new Cards(face, null));
                 } else {
-                    deckOfCards.add(new Cards(face, color));
+                    deckOfCards.push(new Cards(face, color));
                 }
             }
         }
-        for (int i = 0; i < deckOfCards.size(); i++) {
-            System.out.println(deckOfCards.get(i));
+        for (Cards deckOfCard : deckOfCards) {
+//            System.out.println(deckOfCard);
         }
+        Collections.shuffle(deckOfCards);
+        return deckOfCards;
     }
 
+   static class Player{
+        private String name;
+        private int countOfCards = 7;
+        public Stack<Cards> Hand = new Stack<>();
 
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getCountOfCards() {
+            return countOfCards;
+        }
+
+        public void setCountOfCards(int countOfCards) {
+            this.countOfCards = countOfCards;
+        }
+    }
     public record Cards(Main.Cards.Face face, Main.Cards.Color color) implements Comparable<Cards> {
 
 
